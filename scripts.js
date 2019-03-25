8723a8163f61e431bd45716fc35b85e353377f15
Nicole Giura <nicole@Nicoles-MacBook-Air.local>
2019-03-25 17:33:28 +0000

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  $( "#signup_button" ).leanModal({
+  		top: 100,
+  		overlay: 0.6,
+  		closeButton: ".modal_close"
+  });
+});
+/*var shiftWindow = function() { scrollBy(0, -50) };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);*/
